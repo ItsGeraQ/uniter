4d284f94101701aac810352fd43247676b700421
ItsGeraQ <a22216369@alumnos.uady.mx>
2025-04-27 12:40:16 -0600

    fix: correct indentation and formatting in user model

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -2,11 +2,16 @@ from pydantic import BaseModel
 
 
 class UserLogin(BaseModel):
-	username: str
-	password: str
- 
+    username: str
+    password: str
+
+
 class User(BaseModel):
-	username: str
-	password: str
-	email: str
-	age: int
+    username: str
+    password: str
+    email: str
+    firs_name: str
+    second_name: str
+    age: int
+    is_active: bool = True
+    
