274b5599807498c87d8c3931f15b3186f5a4a87f
ItsGeraQ <a22216369@alumnos.uady.mx>
2025-05-04 05:44:58 +0000

    Revert "fear: implement login with amz cognito"
    This reverts commit e1f6b4d2c09848726c6fc13243ac4ef0786a701b.

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
-from app.models.user import UserLogin, User, Confirm
-from app.cruds.user import login_user, register_user, confirm_email, resend_email_code
+from app.models.user import UserLogin, User
+from app.cruds.user import login_user, register_user, recover_password
 from app.db.client import get_db
 
 router = APIRouter()
@@ -11,16 +11,8 @@ async def login(user: UserLogin, db = Depends(get_db)):
     return await login_user(user, db)
 
 @router.post("/signup", tags=["signup"]) 
-async def signup(user: User, db = Depends(get_db)):
-    return await register_user(user, db)
-
-@router.post("/confirm-signup", tags=["confirm-signup"])
-async def confirm_signup(confirm: Confirm, db = Depends(get_db)):
-    return await confirm_email(confirm, db)
-
-@router.post("/resend-code", tags=["resend-code"])
-async def resend_code_confirmation(confirm: Confirm):
-    return await resend_email_code(confirm)
+async def signup(db = Depends(get_db)):
+    return [{"msg": "signup"}]
 
 @router.post("/recovering", tags=["recovering"])
 async def recovering(db= Depends(get_db)):
