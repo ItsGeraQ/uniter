a1f6cf482d14d03cdc4a1b27066be58c7221c188
ItsGeraQ <a22216369@alumnos.uady.mx>
2025-05-04 05:46:38 +0000

    Revert "Revert "feat: implement login with amz cognito""
    This reverts commit 274b5599807498c87d8c3931f15b3186f5a4a87f.

--- app/cruds/user.py
+++ app/cruds/user.py
@@ -1,4 +1,4 @@
-from app.models.user import User, UserLogin
+from app.models.user import User, UserLogin, Confirm
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import HTTPException
 import boto3
@@ -6,28 +6,20 @@ import jwt
 from datetime import timedelta
 from datetime import datetime
 from datetime import timezone
-
-# Configuración de AWS Cognito
-COGNITO_REGION = "us-east-1"  # Cambia según tu región
-COGNITO_USERPOOL_ID = "us-east-1_xQnXpZkzw"  # Reemplaza con tu User Pool ID
-COGNITO_CLIENT_ID = "26qit1a4gue0blmponc2va1n5o"  # Reemplaza con tu Client ID
-COGNITO_CLIENT_SECRET = "1mpf8jevq5ua06r93sg1os6oiohldaksb9spceigj5a6krk7u8uq"  # Reemplaza con tu Client Secret (si aplica)
+from settings import settings
+import base64, hmac, hashlib
+import logging
 
 # Inicializar cliente de Cognito
-cognito_client = boto3.client("cognito-idp", region_name=COGNITO_REGION)
-
-# Configuración JWT
-SECRET_KEY = "your-secret-key"  # Cambia por una clave segura
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
+cognito_client = boto3.client("cognito-idp", region_name=settings.COGNITO_REGION)
+logger = logging.getLogger(__name__)
 
 
 async def login_user(user: UserLogin, db: AsyncIOMotorClient):
     try:
         # Autenticar con Cognito
-        response = cognito_client.initiate_auth(
-            ClientId=COGNITO_CLIENT_ID,
+        cognito = cognito_client.initiate_auth(
+            ClientId=settings.COGNITO_CLIENT_ID,
             AuthFlow="USER_PASSWORD_AUTH",
             AuthParameters={
                 "USERNAME": user.username,
@@ -36,13 +28,14 @@ async def login_user(user: UserLogin, db: AsyncIOMotorClient):
         )
         
         # Obtener tokens
-        id_token = response["AuthenticationResult"]["IdToken"]
-        access_token = response["AuthenticationResult"]["AccessToken"]
+        id_token = cognito["AuthenticationResult"]["IdToken"]
+        access_token = cognito["AuthenticationResult"]["AccessToken"]
+        expires_in = cognito["AuthenticationResult"]["ExpiresIn"]
         
-        # Verificar usuario en MongoDB o crearlo
+        # Verificar usuario en MongoDB o devolver error
         db_user = await db.users.find_one({"username": user.username})
         if not db_user:
-            raise HTTPException(status_code=401, detail="User not found")
+             raise HTTPException(status_code=401, detail="Could not find user in database")
         
         # Crear token JWT local
         token = create_access_token(data={"sub": user.username})
@@ -51,7 +44,8 @@ async def login_user(user: UserLogin, db: AsyncIOMotorClient):
             "access_token": token,
             "token_type": "bearer",
             "cognito_id_token": id_token,
-            "cognito_access_token": access_token
+            "cognito_access_token": access_token,
+            "expires_in": expires_in
         }
     except cognito_client.exceptions.NotAuthorizedException:
         raise HTTPException(status_code=401, detail="Incorrect username or password")
@@ -60,17 +54,119 @@ async def login_user(user: UserLogin, db: AsyncIOMotorClient):
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
 
 async def register_user(user: User, db: AsyncIOMotorClient):
-    pass
+        validate_user_to_register(user, db)
+        # Sign up en Cognito
+        secret_hash = get_secret_hash(user.username)
+        try:
+            response = cognito_client.sign_up(
+                ClientId=settings.COGNITO_CLIENT_ID,
+                SecretHash=secret_hash,
+                Username=user.username,
+                Password=user.password,
+                UserAttributes=[
+                    {
+                        "Name": "name",
+                        "Value": user.first_name    
+                    },
+                    {
+                        "Name": "middle_name",
+                        "Value": user.last_name    
+                    },
+                    {
+                        "Name": "email",
+                        "Value": user.email    
+                    },
+                    {
+                        "Name": "phone_number",
+                        "Value": user.phone_number    
+                    },
+                ]
+                
+            )
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+        
+        if not response["UserConfirmed"]: 
+            raise HTTPException(status_code=500, detail="Error, user not confirmed by Auth provider")
+        
+        user_data = user.model_dump(exclude={"password"})
+        response_db = db.users.insert_one(user_data)
+        if not response_db.inserted_id:
+            raise HTTPException(status_code=500, detail="Error, user not created in database")
+        return response 
 
+async def confirm_email(confirm: Confirm, db: AsyncIOMotorClient):
+    secret_hash = get_secret_hash(confirm.username)
+    try:
+        response = cognito_client.confirm_sign_up(
+            ClientId=settings.COGNITO_CLIENT_ID,
+            SecretHash=secret_hash,
+            Username=confirm.username,
+            ConfirmationCode=confirm.email_code,
+        )
+        update = db.users.update_one(
+            {"username": confirm.username},
+            {"$set": {"is_active": True}}
+        )
+        
+        return response
+    except cognito_client.exceptions.CodeMismatchException:
+        raise HTTPException(status_code=400, detail="Invalid verification code")
+    except Exception as e:
+        raise HTTPException(status_code=501, detail=str(e))
+
+async def resend_email_code(confirm: Confirm):
+    try:
+        secret_hash = get_secret_hash(confirm.username)
+        response = cognito_client.resend_confirmation_code(
+            ClientId=settings.COGNITO_CLIENT_ID,
+            SecretHash=secret_hash,
+            Username=confirm.username,
+        )
+        return response
+    except cognito_client.exceptions.UserNotFoundException:
+        raise HTTPException(status_code=404, detail="User not found")
+    except Exception as e:
+        raise HTTPException(status_code=501, detail=str(e))
+    
 async def recover_password(user: UserLogin, db: AsyncIOMotorClient):
-    pass
+    pass        
+
+def get_secret_hash(username):
+        """
+        Calculates a secret hash from a user name and a client secret.
+
+        :param user_name: The user name to use when calculating the hash.
+        :return: The secret hash.
+        """
+        key = settings.COGNITO_CLIENT_SECRET.encode()
+        msg = bytes(username + settings.COGNITO_CLIENT_ID, "utf-8")
+        secret_hash = base64.b64encode(
+            hmac.new(key, msg, digestmod=hashlib.sha256).digest()
+        ).decode()
+        logger.info("Made secret hash for %s: %s.", username, secret_hash)
+        return secret_hash
+
+
+async def validate_user_to_register(user: User, db: AsyncIOMotorClient):
+    # Find a user where either username OR email matches
+    exists = await db.users.find_one({
+        "$or": [
+            {"username": user.username},
+            {"email": user.email}
+        ]
+    })
+    
+    if exists:
+        raise HTTPException(status_code=400, detail="Username or email already registered")
+
 
 
 
--- app/models/user.py
+++ app/models/user.py
@@ -1,17 +1,49 @@
 from pydantic import BaseModel
-
+from pydantic.networks import EmailStr
+from datetime import datetime, timezone
+from typing import Optional
 
 class UserLogin(BaseModel):
     username: str
     password: str
 
+class Confirm(BaseModel):
+    username: str
+    email_code: Optional[str] = None
+    phone_code: Optional[str] = None
+    mfa_code: Optional[str] = None
 
 class User(BaseModel):
+    created_at: datetime = datetime.now(timezone.utc)
+    updated_at: datetime = datetime.now(timezone.utc)
     username: str
     password: str
-    email: str
-    firs_name: str
-    second_name: str
+    email: EmailStr
+    first_name: str
+    last_name: str
     age: int
-    is_active: bool = True
+    phone_number: str
+    is_active: bool = False
+    
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "username": "XXXXXXX",
+                "password": "XXXXXX",
+                "email": "johndoe@example.com",
+                "first_name": "John",
+                "last_name": "Doe",
+                "age": 30,
+                "phone": "1234567890",
+                "country_code": "+1"
+            }
+        }
+        
+        json_encoders = {
+            datetime: lambda v: v.isoformat()
+        }
+        
+    @classmethod
+    def default_updated_at(cls) -> datetime:
+        return datetime.now(timezone.utc)
     
